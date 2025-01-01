e19f9ec6f073160d2431e3e468af66506c77115b
NeckitWin <neckit.v@gmail.com>
2025-01-01 23:59:58 +0100

    feat: add structure command help

---
+++ src/commands/info/help.ts
@@ -0,0 +1,20 @@
+import {SlashCommandBuilder, CommandInteraction, EmbedBuilder} from "discord.js";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Показывает список всех команд'),
+    async execute(interaction:CommandInteraction) {
+        const embed = new EmbedBuilder()
+            .setTitle('Список команд')
+            .setColor('#0099ff')
+            .setDescription('Список всех команд бота')
+            .addFields(
+                { name: '/help', value: 'Показывает список всех команд' },
+                { name: '/ping', value: 'Показывает задержку бота' }
+            )
+            .setTimestamp();
+
+        await interaction.reply({embeds: [embed]});
+    }
+}

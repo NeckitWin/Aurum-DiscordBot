43ce957d3237476258d1a8bcef52a34845c25851
NeckitWin <neckit.v@gmail.com>
2025-01-01 23:40:42 +0100

    push all

---
+++ dist/events/botRun.js
@@ -0,0 +1,9 @@
+"use strict";
+const { Events } = require('discord.js');
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        console.log(`Aurum is ready!✅`);
+    },
+};

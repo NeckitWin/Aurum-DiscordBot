a22589d8ed7ad1c15aee753ae25ab39b28f3305b
KiNgchev <kingchev.net@gmail.com>
2025-01-02 22:07:54 +0300

    feat: replace from create and update to upsert

--- src/database/repository/userRepository.ts
+++ src/database/repository/userRepository.ts
@@ -8,54 +8,33 @@ module.exports = {
     return client.user.findFirst()
   },
   
-  async insert(id: bigint, username: string, nickname: string, streak: number = 0) {
-    return client.user.create({
-      data: {
-        id: id,
-        username: username,
-        nickname: nickname,
-        streak: streak
-      }
+  async upsert(id: bigint, username: string, nickname: string, streak: number = 0) {
+    let data = {
+      id: id,
+      username: username,
+      nickname: nickname,
+      streak: streak
+    }
+    return client.user.upsert({
+      where: { id: id },
+      create: data,
+      update: data
     })
   },
   
-  async insertUser(user: User) {
-    return client.user.create({
-      data: {
-        id: Number.parseInt(user.id),
-        username: user.username,
-        nickname: user.displayName,
-        streak: 0
-      }
-    })
-  },
-  
-  async update(id: bigint | undefined, username: string | undefined, nickname: string | undefined, streak: number = 0) {
-    client.user.update({
-      where: {
-        id: id
-      },
-      data: {
-        id: id,
-        username: username,
-        nickname: nickname,
-        streak: streak
-      }
-    })
-  },
-  
-  async updateUser(user: User) {
-    let id = Number.parseInt(user.id)
+  async upsertFromUser(user: User, streak: number) {
+    let id = BigInt(user.id)
+    let data = {
+      id: id,
+      username: user.username,
+      nickname: user.displayName,
+      streak: streak
+    }
     
-    client.user.update({
-      where: {
-        id: id
-      },
-      data: {
-        id: id,
-        username: user.username,
-        nickname: user.displayName,
-      }
+    return client.user.upsert({
+      where: { id: id },
+      create: data,
+      update: data
     })
   }
 }
